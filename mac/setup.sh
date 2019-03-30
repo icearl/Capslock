2ff9c6c37b202a8cb47102b7e4cd5d279e984072
icearl <icearl@qq.com>
2019-03-30 18:54:46 +0800

    fix: mouse

---
+++ mac/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#mkdir -p "${HOME}/.config/karabiner/assets/complex_modifications/"
+cp ./caps_lock_3_right_key.json "${HOME}/.config/karabiner/assets/complex_modifications/caps_lock_3_right_key.json"
+echo "${HOME}/.config/karabiner/assets/complex_modifications/caps_lock_3_right_key.json" Done
