01fcb1f4dc2b4213473bbb78779bba9b36c0bf42
icearl <icearl@qq.com>
2020-03-09 19:24:00 +0800

    add: r

--- mac/setup.sh
+++ mac/setup.sh
@@ -3,3 +3,6 @@
 #mkdir -p "${HOME}/.config/karabiner/assets/complex_modifications/"
 cp ./caps_lock_3_right_key.json "${HOME}/.config/karabiner/assets/complex_modifications/caps_lock_3_right_key.json"
 echo "${HOME}/.config/karabiner/assets/complex_modifications/caps_lock_3_right_key.json" Done
+
+cp ./mouse.json "${HOME}/.config/karabiner/assets/complex_modifications/mouse.json"
+echo "${HOME}/.config/karabiner/assets/complex_modiddfications/mouse.json" Done
